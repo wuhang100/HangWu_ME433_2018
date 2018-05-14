48fd517371bc07c1214abc14bf619a3fe057bf90
wuhang100 <36547153+wuhang100@users.noreply.github.com>
2018-05-13 22:37:43 -0500

    Add files via upload

---
+++ HW11/HID/firmware/HID.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HID.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HID.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=hid.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hid.x.tar
